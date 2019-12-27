b0e2d8ceb13cf63a5100b5cd4bb90cb0f5be4391
AKreedz <akreedz@outlook.com>
2019-12-27 17:44:39 +0800

    first

--- Helicopter/Properties/AssemblyInfo.cs
+++ Helicopter/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("CargoPodTransport")]
+[assembly: AssemblyTitle("Helicopter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CargoPodTransport")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Helicopter")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("717e7106-3563-4a8b-bdb1-df80326efc61")]
+[assembly: Guid("9c84a414-98d2-46f9-8469-4ebf0b48c014")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
